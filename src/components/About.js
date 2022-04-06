e68aa482978ac166cf51a7612bee3bbe0325ed42
Juuso Ihatsu <juuso.ihatsu@gmail.com>
2022-04-06 13:26:56 +0300

    Tehtävä valmis

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <p>Home is where your heart is, but where is your heart?</p>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <p>Contact your loved ones, and you'll remember.</p>
+        </div>
+    )
+}
+
+export default Contact;
